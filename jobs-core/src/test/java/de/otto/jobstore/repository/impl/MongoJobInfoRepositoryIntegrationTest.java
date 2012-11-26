fe94c31aadff066f89fb5fcde177e014115fccec
Sebastian Schroeder <sebastian.schroeder@otto.de>
2012-11-26 10:24:47 +0100

    Added TearDownMethod to JobInfoRepoTest

--- jobs-core/src/test/java/de/otto/jobstore/repository/impl/MongoJobInfoRepositoryIntegrationTest.java
+++ jobs-core/src/test/java/de/otto/jobstore/repository/impl/MongoJobInfoRepositoryIntegrationTest.java
@@ -6,6 +6,7 @@ import org.bson.types.ObjectId;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -26,7 +27,7 @@ public class MongoJobInfoRepositoryIntegrationTest extends AbstractTestNGSpringC
     @Resource
     private MongoJobInfoRepository jobInfoRepository;
 
-    @BeforeMethod
+    @BeforeMethod @AfterMethod
     public void setup() throws Exception {
         jobInfoRepository.clear(true);
     }
