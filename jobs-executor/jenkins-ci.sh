5b977b9f364e05cfe0bce6e99860f38e04b8df44
Niko Schmuck <nikolaus.schmuck@otto.de>
2012-12-18 11:54:36 +0100

    separate dir for unit and integration reports

---
+++ jobs-executor/jenkins-ci.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd jobs-executor
+
+scp prepare.sh fred@$server:.
+ssh fred@$server "./prepare.sh $version"
+
+scp start.sh fred@$server:jobs-executor/.
+ssh -n -f fred@$server "sh -c 'cd jobs-executor; nohup ./start.sh > /tmp/jobexec.log 2>&1 &'"
+
+sleep 5
+uname -a
+id
+curl -s -S -i http://$server:5000/
+echo ""
