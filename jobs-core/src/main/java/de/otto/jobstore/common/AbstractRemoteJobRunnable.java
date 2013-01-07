2d929175aeb027ef82d5f5db9e05700fb545ffe5
Niko Schmuck <nikolaus.schmuck@otto.de>
2013-01-07 14:05:57 +0100

    Added logging of job starting

--- jobs-core/src/main/java/de/otto/jobstore/common/AbstractRemoteJobRunnable.java
+++ jobs-core/src/main/java/de/otto/jobstore/common/AbstractRemoteJobRunnable.java
@@ -4,6 +4,8 @@ import de.otto.jobstore.common.properties.JobInfoProperty;
 import de.otto.jobstore.service.RemoteJobExecutorService;
 import de.otto.jobstore.service.exception.JobException;
 import de.otto.jobstore.service.exception.RemoteJobAlreadyRunningException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.List;
 public abstract class AbstractRemoteJobRunnable implements JobRunnable {
 
     private final RemoteJobExecutorService remoteJobExecutorService;
+    protected Logger log = LoggerFactory.getLogger(this.getName());
 
     protected String id;
 
@@ -38,6 +41,7 @@ public abstract class AbstractRemoteJobRunnable implements JobRunnable {
     @Override
     public void execute(JobLogger jobLogger) throws JobException {
         try {
+            log.info("Trigger remote job '{}' [{}]", getName(), getId());
             final URI uri = remoteJobExecutorService.startJob(new RemoteJob(getName(), getId(), getParameters()));
             jobLogger.insertOrUpdateAdditionalData(JobInfoProperty.REMOTE_JOB_URI.val(), uri.toString());
         } catch (RemoteJobAlreadyRunningException e) {
