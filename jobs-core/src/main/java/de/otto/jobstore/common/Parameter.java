bc09471a0a070fa513169302c7a51a142b528aef
Niko Schmuck <nikolaus.schmuck@otto.de>
2013-01-08 19:37:46 +0100

    Made Parameter serializable for Mongo's BSONDecoder

--- jobs-core/src/main/java/de/otto/jobstore/common/Parameter.java
+++ jobs-core/src/main/java/de/otto/jobstore/common/Parameter.java
@@ -4,12 +4,31 @@ import java.io.Serializable;
 
 public final class Parameter implements Serializable {
 
-    public final String key;
-    public final String value;
+    public String key;
+    public String value;
+
+    // TODO: to allow BSONEncoder of Mongo to serialize object
+    public Parameter() {
+    }
 
     public Parameter(String key, String value) {
         this.key = key;
         this.value = value;
     }
 
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
