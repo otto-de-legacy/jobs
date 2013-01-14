0c7ebd3c4ea56ec794a971e83d1995713a1b8e3b
Sebastian Schroeder <sebastian.schroeder@otto.de>
2013-01-14 14:13:45 +0100

    Fixed error were running constraints were not checked.

--- jobs-core/src/main/java/de/otto/jobstore/common/AbstractLocalJobRunnable.java
+++ jobs-core/src/main/java/de/otto/jobstore/common/AbstractLocalJobRunnable.java
@@ -2,6 +2,9 @@ package de.otto.jobstore.common;
 
 import de.otto.jobstore.service.exception.JobException;
 
+import java.util.Collections;
+import java.util.Map;
+
 public abstract class AbstractLocalJobRunnable implements JobRunnable {
 
     @Override
@@ -12,6 +15,11 @@ public abstract class AbstractLocalJobRunnable implements JobRunnable {
     @Override
     public final boolean isRemote() {
         return false;
+    }
+
+    @Override
+    public Map<String, String> getParameters() {
+        return Collections.emptyMap();
     }
 
     /**
