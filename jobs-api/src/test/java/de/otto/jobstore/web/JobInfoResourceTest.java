35b21fd0c528339d97072385216d5739a359f93b
Sebastian Schroeder <sebastian.schroeder@otto.de>
2013-01-17 14:48:05 +0100

    Added additional Tests.

--- jobs-api/src/test/java/de/otto/jobstore/web/JobInfoResourceTest.java
+++ jobs-api/src/test/java/de/otto/jobstore/web/JobInfoResourceTest.java
@@ -30,6 +30,7 @@ import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class JobInfoResourceTest {
 
@@ -185,6 +186,29 @@ public class JobInfoResourceTest {
         assertEquals(200, response.getStatus());
         Map<String, JobInfoRepresentation> history = (Map<String, JobInfoRepresentation>) response.getEntity();
         assertEquals(1, history.size());
+    }
+
+    @Test
+    public void testTogglingJobEnabled() throws Exception {
+        when(jobService.isEnabled("test")).thenReturn(Boolean.TRUE);
+        Response response = jobInfoResource.toggleJobEnabled("test");
+        assertEquals(200, response.getStatus());
+        assertTrue(((String)response.getEntity()).contains("false"));
+    }
+
+    @Test
+    public void testTogglingJobDisabled() throws Exception {
+        when(jobService.isEnabled("test")).thenReturn(Boolean.FALSE);
+        Response response = jobInfoResource.toggleJobEnabled("test");
+        assertEquals(200, response.getStatus());
+        assertTrue(((String)response.getEntity()).contains("true"));
+    }
+
+    @Test
+    public void testTogglingJobDisabledNotRegistered() throws Exception {
+        when(jobService.isEnabled("test")).thenThrow(new JobNotRegisteredException(""));
+        Response response = jobInfoResource.toggleJobEnabled("test");
+        assertEquals(404, response.getStatus());
     }
 
     // ~~
