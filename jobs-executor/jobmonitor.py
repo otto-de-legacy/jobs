73ac34d1e0a24aab1b8cb066231cf27ebaca5380
Niko Schmuck <nikolaus.schmuck@otto.de>
2013-02-11 18:39:29 +0100

    snapshot

--- jobs-executor/jobmonitor.py
+++ jobs-executor/jobmonitor.py
@@ -13,7 +13,7 @@
    own log file in the TRANSCRIPT_DIR.
 """
 
-__version__ = "0.8.26"
+__version__ = "0.8.27"
 __author__  = "Niko Schmuck"
 __credits__ = ["Ilja Pavkovic", "Sebastian Schroeder"]
 
