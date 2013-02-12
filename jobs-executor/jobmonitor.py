44c77cabf0d20dcd38a20329076c9f978a145742
Niko Schmuck <nikolaus.schmuck@otto.de>
2013-02-12 18:16:45 +0100

    pushed build nr

--- jobs-executor/jobmonitor.py
+++ jobs-executor/jobmonitor.py
@@ -13,7 +13,7 @@
    own log file in the TRANSCRIPT_DIR.
 """
 
-__version__ = "0.8.29"
+__version__ = "0.8.30"
 __author__  = "Niko Schmuck"
 __credits__ = ["Ilja Pavkovic", "Sebastian Schroeder"]
 
