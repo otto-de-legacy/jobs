078582601cc9c36aeeeec1f377cd418cfadf7d1e
Christian Herweg <christian.herweg@otto.de>
2013-06-24 16:49:31 +0200

    [jobsexecutor] rpm installation for jobsexecutor

--- generate-rpm.sh
+++ generate-rpm.sh
@@ -9,6 +9,7 @@ CURRENT_VERSION=$(echo ${VERSION} | sed 's/\"//g' | sed 's/\-//g' | sed 's/\SNAP
 #cp git/shit/jobs-executor -> jobexec_rpm
 
 mkdir -p jobexec_rpm/var/opt/jobs-executor
+mkdir -p jobexec_rpm/var/opt/jobs-executor/run
 mkdir -p jobexec_rpm/etc/init.d
 mkdir -p jobexec_rpm/var/spool/jobs-executor/templates
 mkdir -p jobexec_rpm/var/spool/jobs-executor/log
@@ -22,7 +23,7 @@ cp jobs-executor/lru.py jobexec_rpm/var/opt/jobs-executor
 cp jobs-executor/auto_stub.py jobexec_rpm/var/opt/jobs-executor
 cp jobs-executor/version.py jobexec_rpm/var/opt/jobs-executor
 cp jobs-executor/jobmonitor_settings_redhat.cfg jobexec_rpm/var/opt/jobs-executor/jobmonitor_settings.cfg
-cp jobs-executor/jobsmonitor.init jobexec_rpm/etc/init.d/jobsmonitor
+cp jobs-executor/jobs-executor.init jobexec_rpm/etc/init.d/jobs-executor
 cd jobexec_rpm
 
 ls
