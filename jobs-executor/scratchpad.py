6aca181dd482ab237806278c14688cab04ddba6e
Sebastian Schroeder <sebastian.schroeder@otto.de>
2013-10-16 13:39:57 +0200

    Cleaned up repo

--- jobs-executor/scratchpad.py
+++
@@ -1,7 +0,0 @@
-import os, binascii
-
-# create random ascii string for identifying job
-rnd = binascii.b2a_hex(os.urandom(6))
-
-#rv = self.app.post('/jobs/demojob', content_type='application/json', data=json.dumps(payload))
-#resp_js = flask.json.loads(rv.data)
